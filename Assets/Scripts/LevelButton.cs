e4ca6d6e89d5154b508e8778c4c69219d59d442b
Linkinstein <78932362+Linkinstein@users.noreply.github.com>
2023-10-31 21:29:51 +1100

    made script for loading lvl

---
+++ Assets/Scripts/LevelButton.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelButton : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(HandleButtonClick);
+    }
+
+    private void HandleButtonClick()
+    {
+
+        switch (gameObject.name)
+        {
+            case "Bttn_lv1":
+                SceneManager.LoadScene("Level1");
+                break;
+            case "Bttn_lv2":
+                //SceneManager.LoadScene("Level2");
+                break;
+        }
+
+        SceneManager.UnloadSceneAsync("StartScene");
+    }
+}

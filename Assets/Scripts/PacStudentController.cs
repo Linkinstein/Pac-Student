ee0f2894c05f3a64ea675463c602b0ec343aa5bf
Linkinstein <78932362+Linkinstein@users.noreply.github.com>
2023-11-02 01:00:48 +1100

    tryna fix sounds man

--- Assets/Scripts/PacStudentController.cs
+++ Assets/Scripts/PacStudentController.cs
@@ -5,28 +5,56 @@ using UnityEngine.Tilemaps;
 
 public class PacStudentController : MonoBehaviour
 {
+    Animator anim;
+
+    AudioSource audio;
+    public AudioClip collide;
+    public AudioClip eat;
+    public AudioClip step;
+    public AudioClip death;
+
     public float moveSpeed = 10.0f;
     public Tilemap wallTilemap;
-    public Tilemap OrbTilemap;
+    public Tilemap orbTilemap;
     public ParticleSystem walkEffect;
 
     private Vector3 currentInput;
     private Vector3 lastInput;
-    private bool moving;
+    private bool moving = false;
+    private bool start = false;
 
     void Start()
     {
+        anim = GetComponent<Animator>();
+        audio = GetComponent<AudioSource>();
         currentInput = Vector3.zero;
         lastInput = Vector3.zero;
-        moving = false;
     }
 
     void Update()
     {
         if (Input.GetKey(KeyCode.W)) lastInput = Vector3.up;
         if (Input.GetKey(KeyCode.A)) lastInput = Vector3.left;
-        if (Input.GetKey(KeyCode.S)) lastInput = Vector3.down;
-        if (Input.GetKey(KeyCode.D)) lastInput = Vector3.right;
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            lastInput = Vector3.down;
+            if (!start)
+            { 
+                start = true;
+                ChangeAudio(transform.position + lastInput);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.D)) 
+        { 
+            lastInput = Vector3.right;
+            if (!start)
+            {
+                start = true;
+                ChangeAudio(transform.position + lastInput);
+            }
+        }
 
         if (!moving)
         {
@@ -34,9 +62,10 @@ public class PacStudentController : MonoBehaviour
             Vector3 newDir = transform.position + lastInput;
             Vector3 oldDir = transform.position + currentInput;
 
-            if (IsWalkable(newDir))
+            if (IsWalkable(newDir) && newDir != transform.position)
             {
                 currentInput = lastInput;
+                changeAnim();
                 StartCoroutine(LerpToPosition(newDir));
             }
             else if (IsWalkable(oldDir))
@@ -61,6 +90,7 @@ public class PacStudentController : MonoBehaviour
             yield return null;
         }
 
+
         transform.position = target;
         moving = false;
         walkEffect.Stop();
@@ -68,13 +98,67 @@ public class PacStudentController : MonoBehaviour
 
     bool IsWalkable(Vector3 position)
     {
-        // Convert world position to grid position
+        isOrb(position);
         Vector3Int gridPosition = wallTilemap.WorldToCell(position);
-
-        // Check if there is a tile (wall) at the grid position
         TileBase tile = wallTilemap.GetTile(gridPosition);
 
-        // Return false if there's a wall (tile), true if it's walkable (no tile)
-        return tile == null;
+        if (tile != null)
+        {
+            if (lastInput == currentInput)
+            {
+                anim.SetInteger("Direction", 5);
+                ChangeAudio(position);
+            }
+            return false;
+        } else return true;
+    }
+
+    bool isOrb(Vector3 position)
+    {
+        Vector3Int gridPosition = orbTilemap.WorldToCell(position);
+        TileBase tile = orbTilemap.GetTile(gridPosition);
+        Debug.Log(tile);
+        return tile != null;
+    }
+
+    void changeAnim()
+    {
+        if (currentInput == Vector3.up)
+        {
+            anim.SetInteger("Direction", 8);
+        }
+        else if (currentInput == Vector3.left)
+        {
+            anim.SetInteger("Direction", 4);
+        }
+        else if (currentInput == Vector3.down)
+        {
+            anim.SetInteger("Direction", 2);
+        }
+        else if (currentInput == Vector3.right)
+        {
+            anim.SetInteger("Direction", 6);
+        }
+    }
+
+    void ChangeAudio(Vector3 position)
+    {
+        if (!IsWalkable(position))
+        {
+            audio.loop = false;
+            audio.clip = collide;
+        }
+        else if (isOrb(position))
+        {
+            audio.loop = true;
+            audio.clip = eat;
+        }
+        else
+        {
+            audio.loop = true;
+            audio.clip = step;
+        }
+        
+        audio.Play();
     }
 }

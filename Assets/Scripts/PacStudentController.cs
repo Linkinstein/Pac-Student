92de98b606f11d7828cdc885a6b0ca2b36951132
Linkinstein <78932362+Linkinstein@users.noreply.github.com>
2023-11-01 17:49:20 +1100

    added basic lerpmove

--- Assets/Scripts/PacStudentController.cs
+++ Assets/Scripts/PacStudentController.cs
@@ -4,21 +4,50 @@ using UnityEngine;
 
 public class PacStudentController : MonoBehaviour
 {
+    public float moveSpeed = 5.0f;
+
     private Vector3 lastInput = Vector3.zero;
-
-
-    void Start()
-    {
-        
-    }
+    private bool moving = false;
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.W)) lastInput = Vector3.up;
+        if (Input.GetKey("w")) lastInput = Vector3.up;
         if (Input.GetKey(KeyCode.A)) lastInput = Vector3.left;
         if (Input.GetKey(KeyCode.S)) lastInput = Vector3.down;
         if (Input.GetKey(KeyCode.D)) lastInput = Vector3.right;
+        //Debug.Log(lastInput);
+        if (!moving && lastInput != Vector3.zero)
+        {
+            // Check if PacStudent can move in the last input direction
+            Vector3 targetPosition = transform.position + lastInput;
+
+            if (IsWalkable(targetPosition))
+            {
+                StartCoroutine(LerpToPosition(targetPosition));
+            }
+        }
     }
 
+    IEnumerator LerpToPosition(Vector3 target)
+    {
+        moving = true;
+        float journeyLength = Vector3.Distance(transform.position, target);
+        float journeyTime = journeyLength / moveSpeed;
+        float startTime = Time.time;
 
+        while (Time.time < startTime + journeyTime)
+        {
+            float journeyFraction = (Time.time - startTime) / journeyTime;
+            transform.position = Vector3.Lerp(transform.position, target, journeyFraction);
+            yield return null;
+        }
+
+        transform.position = target;
+        moving = false;
+    }
+
+    bool IsWalkable(Vector3 position)
+    {
+        return true;
+    }
 }
